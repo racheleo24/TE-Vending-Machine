2253fdeac7cec2c7c6980085583bcb5c3a98d049
Rachel Leonard <rcleonar92@gmail.com>
2022-11-17 14:40:54 -0500

    Capstone step 6

--- java/src/main/java/com/techelevator/application/VendingMachine.java
+++ java/src/main/java/com/techelevator/application/VendingMachine.java
@@ -16,6 +16,12 @@ public class VendingMachine
 
     private final String FILE1="catering.csv";
     private final String FILE2="catering1.csv";
+    private static BigDecimal currentMoney = new BigDecimal("0.00");
+
+    public static BigDecimal getCurrentMoney() {
+        return currentMoney;
+    }
+
     public void run()
     {
         while(true)
@@ -25,16 +31,16 @@ public class VendingMachine
 
             if(choice.equals("display"))
             {
-                System.out.printf("%-4s %-20s %-10s %-4s\n","ID","Name","Price","Quantity");
+               System.out.printf("%-4s %-20s %-10s %-4s\n","ID","Name","Price","Quantity");
                for(SellableItem item:readFile(FILE1)){
                    System.out.printf("%-4s %-20s $%-10.2f %-4s\n", item.getSlotIdentifier(),item.getName(),item.getPrice(),item.getQuantity());
-
                }
                 // display the vending machine slots
             }
             else if(choice.equals("purchase"))
             {
                 // make a purchase
+                purchaseScreen();
             }
             else if(choice.equals("exit"))
             {
@@ -42,6 +48,8 @@ public class VendingMachine
                 break;
             }
         }
+
+
     }
 
     public List<SellableItem> readFile(String input){
@@ -72,5 +80,29 @@ public class VendingMachine
         }
         return items;
     }
-    
+
+    public void purchaseScreen() {
+        while (true) {
+            String choice = UserInput.getPurchaseScreenOption();
+            Scanner input = new Scanner(System.in);
+
+            if (choice.equals("feed money")) {
+                BigDecimal newMoney = UserInput.feedMoneyInput();
+                currentMoney.add(newMoney);
+
+            } else if (choice.equals("select item")) {
+
+                System.out.printf("%-4s %-20s %-10s %-4s\n", "ID", "Name", "Price", "Quantity");
+                for (SellableItem item : readFile(FILE1)) {
+                    System.out.printf("%-4s %-20s $%-10.2f %-4s\n", item.getSlotIdentifier(), item.getName(), item.getPrice(), item.getQuantity());
+                }
+                // make a purchase
+
+            } else if (choice.equals("finish")) {
+                // good bye
+                break;
+            }
+        }
+    }
+
 }
--- java/src/main/java/com/techelevator/ui/UserInput.java
+++ java/src/main/java/com/techelevator/ui/UserInput.java
@@ -1,5 +1,7 @@
 package com.techelevator.ui;
 
+import com.techelevator.application.VendingMachine;
+
 import java.math.BigDecimal;
 import java.util.Scanner;
 
@@ -46,5 +48,54 @@ public class UserInput
 
     }
 
-    
+    public static String getPurchaseScreenOption()
+    {
+        System.out.println("\nWhat would you like to do?");
+        System.out.println();
+
+        System.out.println("M) Feed Money");
+        System.out.println("S) Select Item");
+        System.out.println("F) Finish Transaction");
+
+        System.out.println();
+        System.out.print("Current Money Provided: " + VendingMachine.getCurrentMoney());
+
+        System.out.println();
+        System.out.print("Please select an option: ");
+
+        String selectedOption = scanner.nextLine();
+        String option = selectedOption.trim().toUpperCase();
+
+        if (option.equals("M"))
+        {
+            return "feed money";
+        }
+        else if (option.equals("S"))
+        {
+            return "select item";
+        }
+        else if (option.equals("F"))
+        {
+            return "finish";
+        }
+        else
+        {
+            return "";
+        }
+
+    }
+
+    public static BigDecimal feedMoneyInput() {
+        System.out.print("\nHow much money would you like to enter? ");
+        String feedMoney = scanner.next();
+
+        if (feedMoney.equals("1") || feedMoney.equals("5") || feedMoney.equals("10") || feedMoney.equals("20")) {
+            BigDecimal newMoney = new BigDecimal(feedMoney);
+            return newMoney;
+        }
+        else {
+            System.out.println("Invalid entry! Taste Elevator only accepts $1, $5, $10, or $20.");
+            return new BigDecimal("0");
+        }
+    }
 }
